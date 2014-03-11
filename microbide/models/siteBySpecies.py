2e5bf5c4b6f8e37a16e20fc52adfbcdaf28ddb12
klocey <ken@weecology.org>
2014-03-11 10:33:08 -0400

    revised siteBySpecies.py in microbe/models directory -KL

--- microbide/models/siteBySpecies.py
+++ microbide/models/siteBySpecies.py
@@ -14,20 +14,34 @@ import coreFunctions as cf
     matrices.
 """
 
-im = 100 # number of individuals immigrating from regional pool per time step
+
+get_siteBySpeciesMatrix(northCOM, southCOM):
+    
+    
+
+
+
 
 num_patches = 20 # number of patches in each local community
 
 lgp = 0.99 # log-series parameter, typically approaches 1 for ecological
-           # communities. represents underlying structure of regional pool.
+           # communities. represents underlying structure of regional pool
 
-state = 'heterogeneous' # homogeneity or heterogeneity among local communities
-            # local communities vary along a single environmental axis, e.g.,
-            # precipitation, exposure, runoff, etc.
+state_list = ['heterogeneous','homogeneous'] # homogeneity or heterogeneity 
+            # among local communities local communities vary along a single 
+            # environmental axis, e.g.,mean daily temp, precipitation, etc.
 
-time = 100 # number of time steps. 
+time_list = [100,500,1000] # number of time steps 
+im_list = [100, 200, 400]  # number of individuals immigrating
+                           # from regional pool per time step
 
-
-northCOM, southCOM = cf.microbide(im, num_patches, lgp, state, time)
-
-print len(northCOM),'patches in north and', len(southCOM),'patches in south'
+for state in state_list:
+    for im in im_list:
+        for time in time_list:
+            
+            northCOM, southCOM = cf.microbide(im, num_patches, lgp, state, time)
+            print len(northCOM),'patches in north and',
+            print len(southCOM),'patches in south'
+            
+            siteBySpeciesMatrix = get_siteBySpeciesMatrix(northCOM, southCOM)
+            
