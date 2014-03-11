6c554b0607e3c61f538b27b126a9d946280bd1e2
klocey <ken@weecology.org>
2014-03-11 15:04:50 -0400

    revised siteBySpecies.py and coreFunctions.py -KL

--- microbide/models/siteBySpecies.py
+++ microbide/models/siteBySpecies.py
@@ -14,34 +14,35 @@ import coreFunctions as cf
     matrices.
 """
 
-
-get_siteBySpeciesMatrix(northCOM, southCOM):
-    
-    
-
-
-
-
 num_patches = 20 # number of patches in each local community
 
 lgp = 0.99 # log-series parameter, typically approaches 1 for ecological
            # communities. represents underlying structure of regional pool
 
-state_list = ['heterogeneous','homogeneous'] # homogeneity or heterogeneity 
+state_list = ['heterogeneous']#,'homogeneous'] # homogeneity or heterogeneity 
             # among local communities local communities vary along a single 
             # environmental axis, e.g.,mean daily temp, precipitation, etc.
 
-time_list = [100,500,1000] # number of time steps 
-im_list = [100, 200, 400]  # number of individuals immigrating
+im_list = [300]  # number of individuals immigrating
                            # from regional pool per time step
 
+time = 200
 for state in state_list:
     for im in im_list:
-        for time in time_list:
+        
+        northCOM, southCOM = cf.microbide(im, num_patches, lgp, state, time)
+        print len(northCOM),'patches in north and',
+        print len(southCOM),'patches in south'
             
-            northCOM, southCOM = cf.microbide(im, num_patches, lgp, state, time)
-            print len(northCOM),'patches in north and',
-            print len(southCOM),'patches in south'
+        SbyS = cf.get_SitebySpecies([northCOM, southCOM])
             
-            siteBySpeciesMatrix = get_siteBySpeciesMatrix(northCOM, southCOM)
+        path = '/Users/lisalocey/Desktop/evolution-canyon/microbide/models/'
+        fileName = 'SiteBySpecies_' + str(state) + 'immigration=' + str(im)
+        OUT = open(path + fileName + '.txt','w+')
+        
+        #for row in SbyS:
+        #    print row
+        sys.exit()
+        
+            
             
