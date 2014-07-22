633987fbe5bd1dfec698a91dc485f2fda477f43f
Mario Muscarella <mmuscare@indiana.edu>
2014-07-22 12:57:21 -0400

    minor changes
    Former-commit-id: 1634a110f7f21d7467efaf41c65aa69e43bc6cda

--- stats/EC_diversity.R
+++ stats/EC_diversity.R
@@ -6,7 +6,7 @@
 #                                                                              #
 # Written by: Mario Muscarella                                                 #
 #                                                                              #
-# Last update: 2014/07/09                                                      #
+# Last update: 2014/07/11                                                      #
 #                                                                              #
 ################################################################################
 #                                                                              #
@@ -32,47 +32,52 @@ se <- function(x, ...){sd(x, ...)/sqrt(length(x))}
 
 # Define Inputs and Parameters
 design <- "./data/design.txt"
-shared <- "./mothur/EC.bac.final.shareSd"
+shared <- "./mothur/EC.bac.final.shared"
 level <- "0.03"
-size   <- 10000
+size   <- 50000
 
 # Import Design File
 design <- read.delim(design, header=T, row.names=1)
-design <- 
 slope.molecule <- data.frame(cbind(as.character(design$slope),
   as.character(design$molecule))) # Y matrix with factor 1 and 2
-slope.molecule.concat <- do.call(paste, c(slope.molecule[c("X1", "X2")],
+design$slope.molecule.concat <- do.call(paste, c(slope.molecule[c("X1", "X2")],
   sep = "")) # create unique treat ID vector
 
 # Calculate Sampling Depth & Coverage
 counts <- count.groups(read.otu(shared, cutoff = level))
 coverage <- coverage(input= shared, cutoff = level, size = size)
+counts2 <- counts[counts > 50000]
+
+design <- design[names(counts2), ]
   
 # Alpha Diversity with Resampling
 rich <- round(richness.iter(input = shared, cutoff = level, size = size,
   iters = 100), 3)
+rich <- rich[names(counts2),]
 even <- round(evenness.iter(input = shared, cutoff = level, size = size,
   iters = 100), 3)
+even <- even[names(counts2),]
 shan <- round(diversity.iter(input = shared, index = "shannon",
   cutoff = level, size = size, iters = 100), 3)
+shan <- shan[names(counts2),]
 
 # Richness Summary
-rich_data <- merge(design, rich, by="row.names")
-rich_data$mean <- round(apply(rich_data[1:76], 1, mean, na.rm = TRUE),3)
-rich_data$se <- round(apply(rich_data[1:76], 1, se, na.rm = TRUE), 3)
-rich_data$Design <- slope.molecule.concat
+rich_data <- merge(design[,1:4], rich, by="row.names")
+rich_data$mean <- round(apply(rich, 1, mean, na.rm = TRUE),3)
+rich_data$se <- round(apply(rich, 1, se, na.rm = TRUE), 3)
+rich_data$Design <- as.factor(design$slope.molecule.concat)
 
 # Evenness Summary
-even_data <- merge(design, even, by="row.names")
-even_data$mean <- round(apply(even_data[3:26], 1, mean, na.rm = TRUE),3)
-even_data$se <- round(apply(even_data[3:26], 1, se, na.rm = TRUE),3)
-even_data$Design <- slope.molecule.concat
+even_data <- merge(design[,1:4], even, by="row.names")
+even_data$mean <- round(apply(even, 1, mean, na.rm = TRUE),3)
+even_data$se <- round(apply(even, 1, se, na.rm = TRUE),3)
+even_data$Design <- as.factor(design$slope.molecule.concat)
 
 # Shannon Diversity Summary
-shan_data <- merge(design, shan, by="row.names")
-shan_data$mean <- round(apply(shan_data[3:26], 1, mean, na.rm = TRUE),3)
-shan_data$se <- round(apply(shan_data[3:26], 1, se, na.rm = TRUE),3)
-shan_data$Design <- slope.molecule.concat
+shan_data <- merge(design[,1:4], shan, by="row.names")
+shan_data$mean <- round(apply(shan, 1, mean, na.rm = TRUE),3)
+shan_data$se <- round(apply(shan, 1, se, na.rm = TRUE),3)
+shan_data$Design <- as.factor(design$slope.molecule.concat)
 
 # Plot Parameters
 windows.options(width=6, height=12)
@@ -83,11 +88,11 @@ rich.means <- tapply(rich_data$mean, rich_data$Design, mean)
 rich.se <- tapply(rich_data$mean, rich_data$Design, se)
 dim.r <- round(c(min(rich.means)-(max(rich.se)*1.1),
   max(rich.means)+(max(rich.se)*1.1)),2)
-plot(rich.means, ylim=dim.r, xlim=c(0.5,6.5), pch=15, cex=2, yaxt="n",
+plot(rich.means, ylim=dim.r, xlim=c(0.5,4.5), pch=15, cex=2, yaxt="n",
   cex.lab=2, cex.axis=1.25, las=1, xaxt="n", ylab="")
-arrows(x0 = 1:6, y0 = rich.means, y1 = rich.means - rich.se, angle = 90,
+arrows(x0 = 1:4, y0 = rich.means, y1 = rich.means - rich.se, angle = 90,
   length=0.05, lwd = 2)
-arrows(x0 = 1:6, y0 = rich.means, y1 = rich.means + rich.se, angle = 90,
+arrows(x0 = 1:4, y0 = rich.means, y1 = rich.means + rich.se, angle = 90,
   length=0.05, lwd = 2)
 title(ylab = "Taxa Richness", cex.lab = 2, line = 3.5)
 box(lwd=2)
@@ -103,11 +108,11 @@ even.means <- tapply(even_data$mean, even_data$Design, mean)
 even.se <- tapply(even_data$mean, even_data$Design, se)
 dim.e <- round(c(min(even.means)-(max(even.se)*1.1),
   max(even.means)+(max(even.se)*1.1)),2)
-plot(even.means, ylim=dim.e, xlim=c(0.5,6.5), pch=15, cex=2, yaxt="n",
+plot(even.means, ylim=dim.e, xlim=c(0.5,4.5), pch=15, cex=2, yaxt="n",
   cex.lab=2, cex.axis=1.25, las=1, xaxt="n", ylab="")
-arrows(x0 = 1:6, y0 = even.means, y1 = even.means - even.se, angle = 90,
+arrows(x0 = 1:4, y0 = even.means, y1 = even.means - even.se, angle = 90,
   length=0.05, lwd = 2)
-arrows(x0 = 1:6, y0 = even.means, y1 = even.means + even.se, angle = 90,
+arrows(x0 = 1:4, y0 = even.means, y1 = even.means + even.se, angle = 90,
   length=0.05, lwd = 2)
 title(ylab = "Taxa Evenness", cex.lab = 2, line = 3.5)
 box(lwd=2)
@@ -123,16 +128,16 @@ shan.means <- tapply(shan_data$mean, shan_data$Design, mean)
 shan.se <- tapply(shan_data$mean, shan_data$Design, se)
 dim.s <- round(c(min(shan.means)-(max(shan.se)*1.1),
   max(shan.means)+(max(shan.se)*1.1)),2)
-plot(shan.means, ylim=dim.s, xlim=c(0.5,6.5), pch=15, cex=2, yaxt="n",
+plot(shan.means, ylim=dim.s, xlim=c(0.5,4.5), pch=15, cex=2, yaxt="n",
   cex.lab=2, cex.axis=1.25, las=1, xaxt="n", ylab="")
 points(shan.means, pch=15, cex=2)
-arrows(x0 = 1:6, y0 = shan.means, y1 = shan.means - shan.se, angle = 90,
+arrows(x0 = 1:4, y0 = shan.means, y1 = shan.means - shan.se, angle = 90,
   length=0.05, lwd = 2)
-arrows(x0 = 1:6, y0 = shan.means, y1 = shan.means + shan.se, angle = 90,
+arrows(x0 = 1:4, y0 = shan.means, y1 = shan.means + shan.se, angle = 90,
   length=0.05, lwd = 2)
 title(ylab = "Shannon Diversity", cex.lab = 2, line = 3.5)
 box(lwd=2)
-axis(side = 1, labels=c('Ctrl', 'SRP', 'AEP', 'ATP', 'Phyt', 'Mix'), at=1:6,
+axis(side = 1, labels=c('AFDNA', 'AFRNA', 'EUDNA', 'EURNA'), at=1:4,
   lwd.ticks=2, cex.axis=1.25)
 axis(side = 2, labels=T, lwd.ticks=2, las=1, cex.axis=1.25)
 axis(side = 1, tck=0.02, labels=F, lwd.ticks=2)
@@ -141,4 +146,30 @@ axis(side = 3, tck=0.02, labels=F, lwd.ticks=2)
 axis(side = 4, tck=0.02, labels=F, lwd.ticks=2)
 
 
+# Plot Parameters
+dev.off()
+windows.options(reset=T)
+windows.options(width=6, height=6)
+par(mar=c(0.25,5.5,0.25,0.25), oma=c(3,1,1,1)+0.1 )
 
+# Richness Plot A
+rich.means <- tapply(rich_data$mean, rich_data$Design, mean)
+rich.se <- tapply(rich_data$mean, rich_data$Design, se)
+dim.r <- round(c(min(rich.means)-(max(rich.se)*1.1),
+  max(rich.means)+(max(rich.se)*1.1)),2)
+plot(rich.means, ylim=dim.r, xlim=c(0.5,4.5), pch=15, cex=2, yaxt="n",
+  cex.lab=2, cex.axis=1.25, las=1, xaxt="n", ylab="")
+arrows(x0 = 1:4, y0 = rich.means, y1 = rich.means - rich.se, angle = 90,
+  length=0.05, lwd = 2)
+arrows(x0 = 1:4, y0 = rich.means, y1 = rich.means + rich.se, angle = 90,
+  length=0.05, lwd = 2)
+title(ylab = "Taxa Richness", cex.lab = 2, line = 4)
+axis(side = 1, labels=c('AFDNA', 'AFRNA', 'EUDNA', 'EURNA'), at=1:4,
+  lwd.ticks=2, cex.axis=1.25)
+box(lwd=2)
+axis(side = 1, labels=F, lwd.ticks=2)
+axis(side = 2, labels=T, lwd.ticks=2, las=1, cex.axis=1.25)
+axis(side = 1, tck=0.02, labels=F, lwd.ticks=2)
+axis(side = 2, tck=0.02, labels=F, lwd.ticks=2)
+axis(side = 3, tck=0.02, labels=F, lwd.ticks=2)
+axis(side = 4, tck=0.02, labels=F, lwd.ticks=2)
