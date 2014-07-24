0d2efe097128ecf1b36484d5ae7b422b83fd5a85
Mario Muscarella <mmuscare@indiana.edu>
2014-07-24 11:14:30 -0400

    Calculation for %Dormancy now working. Also added second equation
    Former-commit-id: 9d3af1c2e2b84c8e0cca11164cbba5d853179aaf

--- stats/EC_dormancy.R
+++ stats/EC_dormancy.R
@@ -65,47 +65,105 @@ dataREL <- ec_data
     dataREL[,i] = ec_data[,i]/sum(ec_data[,i])
     }
 
-# Calculate Dormancy
+# Calculate Dormancy (Equation # 1)
 
-  sites <- levels(groups$X2)
-  per.d <- rep(NA, length(sites))
-  total <- rep(NA, length(sites))
-  active <- rep(NA, length(sites))
-  slope <- as.character(design[design$molecule == "DNA",]$slope)
-   
-  for (i in 1:length(sites)){
-  
-  site <- groups$X1[groups$X2 == sites[i]]
-  
-  if (length(site) == 1) {
-  
-    print(paste(sites[i], "is not paired", sep=" "))   
-    } else {
+sites  <- levels(groups$X2)
+per.d  <- rep(NA, length(sites))
+total  <- rep(NA, length(sites))
+active <- rep(NA, length(sites))
+slope  <- as.character(design[design$molecule == "DNA",]$slope)
     
-      if (length(site) == 2) {
-      
-
-  X.tmp1 <- t(subset(dataPA, row.names(dataPA) %in% site)) 
-  X.tmp2 <- X.tmp[which(rowSums(X.tmp1) != 0),]  
-  tot <- dim(X.tmp2)[1]
-  total[i] <- tot
-  act <- sum(X.tmp2[,2])
-  active[i] <- act
-  per.d[i] <- 1 - (act/tot)
-
-  
+for (i in 1:length(sites)){
+  site <- groups$X1[groups$X2 == sites[i]]
+  if (length(site) == 1) {
+    print(paste(sites[i], "is not paired", sep=" "))   
   } else {
-  print(paste(sites[i], "contains more than two samples", sep=" "))
-  }}}
+    if (length(site) == 2) {
+      if (length(which(row.names(dataPA) %in% site)) == 2) {
+      X.tmp1 <- t(subset(dataPA, row.names(dataPA) %in% site)) 
+      X.tmp2 <- X.tmp1[which(rowSums(X.tmp1) != 0),]  
+      tot <- dim(X.tmp2)[1]
+      total[i] <- tot
+      act <- sum(X.tmp2[,2])
+      active[i] <- act
+      per.d[i] <- 1 - (act/tot)
+    } else {
+      print(paste(sites[i], "is not paired", sep=" "))
+    }
+    } else {
+      print(paste(sites[i], "contains more than two samples", sep=" "))
+    }}}
 
-out <- data.frame(sites, slope, as.numeric(total), as.numeric(active), per.d = as.numeric(round(per.d, 2)))
+out <- data.frame(sites, slope, as.numeric(total), as.numeric(active), 
+                  per.d = as.numeric(round(per.d, 2)))
 
-plot(out$slope, out$per.d, ylab = "Dormancy")
+levels(out$slope) <- c("South Facing Slope", "North Facing Slope")
+
+# Plot Parameters
+windows.options(width=6, height=6)
+par(mar=c(2.5,5,1,1), oma=c(1,1,1,1)+0.1 )
+
+# Initiate Plot
+plot(out$slope, out$per.d, xaxt="n", yaxt="n",las=1, frame.plot=F)
+
+axis(side = 1, labels=levels(out$slope), lwd.ticks=0, las=1, cex.axis=1.2, at=c(1,2))
+axis(side = 2, labels=T, lwd.ticks=2, las=2, cex.axis=1.2, at=c(seq(0,1,by=0.1)))
+title(ylab = "Dormancy", cex.lab = 1.5, line = 3.5)
+box(lwd=2, bty="l")
+legend("topright", "Equation 1", bty="n")
 
 
 
+# Calculate Dormancy (Equation # 2)    
+
+sites  <- levels(groups$X2)
+per.d  <- rep(NA, length(sites))
+total  <- rep(NA, length(sites))
+active <- rep(NA, length(sites))
+slope  <- as.character(design[design$molecule == "DNA",]$slope)
+    
+for (i in 1:length(sites)){
+  site <- groups$X1[groups$X2 == sites[i]]
+  if (length(site) == 1) {
+    print(paste(sites[i], "is not paired", sep=" "))   
+  } else {
+    if (length(site) == 2) {
+      if (length(which(row.names(dataPA) %in% site)) == 2) {
+      X.tmp1 <- t(subset(dataPA, row.names(dataPA) %in% site)) 
+      X.tmp2 <- X.tmp1[which(X.tmp1[,1] != 0),]  
+      tot <- dim(X.tmp2)[1]
+      total[i] <- tot
+      act <- sum(X.tmp2[,2])
+      active[i] <- act
+      per.d[i] <- 1 - (act/tot)
+    } else {
+      print(paste(sites[i], "is not paired", sep=" "))
+    }
+    } else {
+      print(paste(sites[i], "contains more than two samples", sep=" "))
+    }}}
+
+out <- data.frame(sites, slope, as.numeric(total), as.numeric(active), 
+                  per.d = as.numeric(round(per.d, 2)))
+
+levels(out$slope) <- c("South Facing Slope", "North Facing Slope")
+
+# Plot Parameters
+windows()
+windows.options(width=6, height=6)
+par(mar=c(2.5,5,1,1), oma=c(1,1,1,1)+0.1 )
+
+# Initiate Plot
+plot(out$slope, out$per.d, xaxt="n", yaxt="n",las=1, frame.plot=F)
+
+axis(side = 1, labels=levels(out$slope), lwd.ticks=0, las=1, cex.axis=1.2, at=c(1,2))
+axis(side = 2, labels=T, lwd.ticks=2, las=2, cex.axis=1.2, at=c(seq(0,1,by=0.1)))
+title(ylab = "Dormancy", cex.lab = 1.5, line = 3.5)
+box(lwd=2, bty="l")
+legend("topright", "Equation 2", bty="n")
 
 
+# Dormancy Functions (Not developed Yet)
 
 
 dormancy <- function(X = "", groupings = ""){
