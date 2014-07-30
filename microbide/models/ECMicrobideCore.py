revised Python scripts in microbide/Models directory. Fixed deep-seated bugs in how environmental conditions influence life history processes -KL
Former-commit-id: f1ebc337c0555804c21a0aba54ffa1a5deb94f50

microbide/models/ECMicrobideCore.py
@@ -1,18 +1,21 @@
 from __future__ import division 
 from random import choice, randrange
 import numpy as np
+import sys
 
 import ECfunctions as funx
 
 
 
-def immigration(COM, oDict, dDict, im):    
+def immigration(COM, oDict1, oDict2, dDict, im):    
     
     propagules = np.random.logseries(0.97, im)  # list of propagules 
     for p in propagules:
-        if p not in oDict:
-            oDict[p] = np.random.uniform(0.001, 0.99, 1).tolist()[0]
-            dDict[p] = np.random.uniform(0.111, 0.99, 1).tolist()[0]
+        if p not in oDict1:
+            
+            oDict1[p] = np.random.uniform(0.0001, 0.9999, 1).tolist()[0]
+            oDict2[p] = np.random.uniform(0.0001, 0.9999, 1).tolist()[0]
+            dDict[p] =  np.random.uniform(0.5000, 0.9999, 1).tolist()[0]
             
         x = np.random.uniform(0.001, 0.99, 1).tolist()[0]
         y = np.random.uniform(0.001, 0.99, 1).tolist()[0]
@@ -30,34 +33,41 @@ def microbide(combo, Ncounts, Nverts, Scounts, Sverts, ic):
     Nmax = max(Ncounts) # max heat value
     Smax = max(Scounts) # max heat value
     
-    oDict, dDict, COM = [ {}, {}, [] ]
+    #print Nmax, Smax
+    #sys.exit()
     
-    N = 2*10**5 # Starting total abundance across the landscape
-    COM = immigration(COM, oDict, dDict, N)
+    oDict1, oDict2, dDict, COM = [ {}, {}, {}, [] ]
     
-    for t in range(2*10**6):
+    N = 2 * 10**6 # Starting total abundance across the landscape
+    COM = immigration(COM, oDict1, oDict2, dDict, N)
+    
+    for t in range(10**6):
         
         """ Immigration """
-        x = np.random.binomial(1, 0.2)
+        p = 0.0 # probability of an individual immigrating
+        x = np.random.binomial(1, p) 
         if x == 1:
-            COM = immigration(COM, oDict, dDict, 1)
+            COM = immigration(COM, oDict1, oDict2, dDict, 1)
     
     
         """ Death """
         i = randrange(len(COM))
-        spID, state, x1, y1, opt = [COM[i][0], COM[i][1], COM[i][2], 
-                                            COM[i][3], oDict[COM[i][0]]]
+        ind = COM[i]
+        
+        spID, state, x1, y1 = [ind[0], ind[1], ind[2], ind[3]]
+        opt1, opt2 =  oDict1[ind[0]], oDict2[ind[0]]
         
         match = funx.get_match(Nverts, Sverts, x1, y1, Ncounts,
-                                    Nmax, Scounts, Smax, opt)
+                                    Nmax, Scounts, Smax, opt1, opt2)
         
-        if state == 0:
+        if state == 0: # if the individual is active
             x = np.random.binomial(1, match)
             if x == 0: COM.pop(i) # likely to die from a low match    
 	
-	elif state == 1:
+	elif state == 1: # if the individual is dormant
             d = dDict[spID]
-            x = np.random.binomial(1, match) # multiplicative interaction between dormancy decay and mismatch to env.
+            x = np.random.binomial(1, match) # multiplicative interaction between
+                                            #dormancy decay and mismatch to env.
             if x == 0: 
                 x = np.random.binomial(1, d) # 'second chance' from dormancy
                 if x == 1:
@@ -66,59 +76,67 @@ def microbide(combo, Ncounts, Nverts, Scounts, Sverts, ic):
 	""" Reproduction """
 	i = randrange(len(COM))
         ind = COM[i]
-        spID, state, x1, y1, opt = [ind[0], ind[1], ind[2], 
-                                            ind[3], oDict[ind[0]]]
         
+        spID, state, x1, y1 = [ind[0], ind[1], ind[2], ind[3]]
+        opt1, opt2 =  oDict1[ind[0]], oDict2[ind[0]]
+         
         match = funx.get_match(Nverts, Sverts, x1, y1, Ncounts,
-                                    Nmax, Scounts, Smax, opt)
+                                    Nmax, Scounts, Smax, opt1, opt2)
         
         x = np.random.binomial(1, match)
-        if state==0 and x==1: COM.append(COM[i]) # reproduce from a high match   
-	
+        if state == 0 and x == 1:
+            COM.append(COM[i]) # reproduce from a high match   
+	    #print 'reproduce with match of,', match
 	    
 	""" Transition between activity and dormancy """
 	i = randrange(len(COM))
         ind = COM[i]
-        spID, state, x1, y1, opt = [ind[0], ind[1], ind[2], 
-                                            ind[3], oDict[ind[0]]]
+        
+        spID, state, x1, y1 = [ind[0], ind[1], ind[2], ind[3]]
+        opt1, opt2 =  oDict1[ind[0]], oDict2[ind[0]]
         
         if state == 0: # if active
             match = funx.get_match(Nverts, Sverts, x1, y1, Ncounts, 
-                                    Nmax, Scounts, Smax, opt)
+                                    Nmax, Scounts, Smax, opt1, opt2)
             
-            if enterD == 'random':
+            if enterD == 'rand':
+                
                 match1 = 0
 	        while match1 <=0 or match1 >=1: 
-	           match1 = np.random.normal(match, 0.1, 1)
+	           match1 = np.random.uniform(0, 1) 
 	        match = float(match1)
 	    
 	    x = np.random.binomial(1, match)
-	    if x == 0: COM[i][1] = 1 # go dormant with a low match              
-	
+	    if x == 0:
+	        COM[i][1] = 1 # go dormant with a low match              
+	        #print 'go dormant with a match of', round(match,2)
+	       
 	if state == 1: # if dormant
 	    match = funx.get_match(Nverts, Sverts, x1, y1, Ncounts, 
-                                    Nmax, Scounts, Smax, opt)
+                                    Nmax, Scounts, Smax, opt1, opt2)
             
-            if exitD == 'random': 
+            if exitD == 'rand': 
+	        
 	        match1 = 0
-	        while match1 <=0 or match1 >=1: 
-	            match1 = np.random.normal(match, 0.1, 1)
+	        while match1 <=0 or match1 >=1:
+	            match1 = np.random.uniform(0, 1)
 	        match = float(match1)
 	    
 	    x = np.random.binomial(1, match)
-	    if x == 1: COM[i][1] = 0 # activate with a high match   
-	    
+	    if x == 1:
+	        COM[i][1] = 0 # activate with a high match   
+	        #print 'go active with a match of', round(match,2)
 	                          
 	""" Dispersal """
         i = randrange(len(COM))
-        x = np.random.normal(loc=COM[i][2], scale=0.1)     
-	y = np.random.normal(loc=COM[i][3], scale=0.1)
+        x = np.random.normal(loc=COM[i][2], scale=0.01)     
+	y = np.random.normal(loc=COM[i][3], scale=0.01)
 	
 	if x > 1 or x < 0: COM.pop(i)
 	elif y > 1 or y < 1: COM.pop(i)
 	 
         t += 1
-        if t%10000 == 0:
+        if t%1000 == 0:
             print 'condition',ic+1,' time:',t, 'N:', len(COM)            
     
     return COM    

microbide/models/ECfunctions.py
@@ -1,6 +1,4 @@
 from __future__ import division 
-import matplotlib
-matplotlib.use('Agg')
 import numpy as np
 from scipy.stats import gaussian_kde
 import sys
@@ -35,10 +33,10 @@ def e_var(SAD):
     return(evar)
     
     
-def get_match(Nverts, Sverts, x1, y1, Ncounts, Nmax, Scounts, Smax, opt): 
+def get_match(Nverts, Sverts, x1, y1, Ncounts, Nmax, Scounts, Smax, opt1, opt2): 
 
     dmin = 10**6
-    Nenv = 0
+    Nval = 0
         
     for j, vert in enumerate(Nverts):
         x2, y2 = vert
@@ -46,10 +44,10 @@ def get_match(Nverts, Sverts, x1, y1, Ncounts, Nmax, Scounts, Smax, opt):
         dist = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
         if dist < dmin:
             dmin = dist
-            Nenv = np.abs(opt - Ncounts[j]/Nmax) # mismatch to North niche
+            Nval = np.abs(opt1 - Ncounts[j]/Nmax) # mismatch to North niche
     
     dmin = 10**6
-    Senv = 0
+    Sval = 0
     
     for j, vert in enumerate(Sverts):
         x2, y2 = vert
@@ -57,9 +55,9 @@ def get_match(Nverts, Sverts, x1, y1, Ncounts, Nmax, Scounts, Smax, opt):
         dist = np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
         if dist < dmin:
             dmin = dist
-            Senv = np.abs(opt - Scounts[j]/Smax) # mismatch to South niche
+            Sval = np.abs(opt2 - Scounts[j]/Smax) # mismatch to South niche
     
-    match = 1 - (Senv * Nenv)
+    match = 1 - (np.mean([Sval, Nval])) # average match to the environment
     return match
     
 