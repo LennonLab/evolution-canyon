2b08ed1fdac4313290cc13ac65a759c187490ddb
klocey <ken@weecology.org>
2014-07-30 13:46:16 -0400

    divided ECmodel.py into smaller modules, modified spatial configuration of plots -KL
    Former-commit-id: 55f16981360a2b65b766f92e67fcc02e35ecf1c6

---
+++ microbide/models/ECfigure.py
@@ -0,0 +1,116 @@
+from __future__ import division 
+from random import sample
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt 
+import numpy as np
+
+import ECfunctions as funx
+
+def ECfig(envdiff):
+    
+    n = 1 * 10**4
+    envDiff = 'differ' # These two statement are used for informal testing
+    #envDiff = 'same'
+    
+    Alpha, Beta = 10, 2
+    Nx = np.random.beta(Alpha, Beta, n).tolist()
+    Nx = filter(lambda a: a >= 0.5, Nx)
+    Sx = np.random.beta(Beta, Alpha, n).tolist()
+    Sx = filter(lambda a: a <= 0.5, Sx)
+        
+    Ny = np.random.uniform(0, 1, len(Nx)).tolist()
+    Sy = np.random.uniform(0, 1, len(Sx)).tolist()
+    
+    fig = plt.figure()
+    fig.add_subplot(2, 1, 1)
+    
+    if envDiff == 'differ':
+        plt.hexbin(Nx, Ny, mincnt=0, cmap=plt.cm.jet, gridsize = 20, alpha=0.4)
+        plt.hexbin(Sx, Sy, mincnt=0, cmap=plt.cm.jet, gridsize = 20, alpha=0.4)
+    
+        imageN = plt.hexbin(Nx, Ny, mincnt=0, gridsize = 20, bins = 'log', cmap=plt.cm.YlOrBr, alpha=0.6)
+        imageS = plt.hexbin(Sx, Sy, mincnt=0, gridsize = 20, bins = 'log', cmap=plt.cm.YlGn, alpha=0.6)
+    
+    elif envDiff == 'same':
+        plt.hexbin(Nx, Ny, mincnt=0, cmap=plt.cm.jet, gridsize = 20, alpha=0.4)
+        plt.hexbin(Sx, Sy, mincnt=0, cmap=plt.cm.jet, gridsize = 20, alpha=0.4)
+        
+        imageN = plt.hexbin(Nx, Ny, mincnt=0, gridsize = 20, bins = 'log', cmap=plt.cm.YlGn, alpha=0.6)
+        imageS = plt.hexbin(Sx, Sy, mincnt=0, gridsize = 20, bins = 'log', cmap=plt.cm.YlGn, alpha=0.6)
+                
+    Ncounts = imageN.get_array()
+    Nverts = imageN.get_offsets()
+    Nverts = sample(Nverts, 10)
+    Nverts = np.array(Nverts)
+    
+    Scounts = imageS.get_array()
+    Sverts = imageS.get_offsets()
+    Sverts = sample(Sverts, 10)
+    Sverts = np.array(Sverts)
+    
+    NRow1Xs = [0.06] * 10  
+    NRow2Xs = [0.12] * 10
+    NRowYs = [0.26, 0.32, 0.38, 0.44, 0.50, 0.56, 0.62, 0.68, 0.74, 0.80] 
+    
+    SRow1Xs = [0.88] * 10  
+    SRow2Xs = [0.94] * 10
+    SRowYs = [0.26, 0.32, 0.38, 0.44, 0.50, 0.56, 0.62, 0.68, 0.74, 0.80]
+    
+    plt.scatter(NRow1Xs, NRowYs, color='w', s=20, alpha=0.5)							
+    plt.scatter(NRow2Xs, NRowYs, color='w', s=20, alpha=0.5)							
+    
+    plt.scatter(SRow1Xs, SRowYs, color='Cyan', s=20, alpha=0.5)							
+    plt.scatter(SRow2Xs, SRowYs, color='Cyan', s=20, alpha=0.5)							
+    
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    
+    plt.ylabel('West to East', fontsize=16)
+    plt.xlabel('South                                               North', fontsize=14)
+    
+    if envDiff == 'differ':
+        title = 'Birds-eye view of the environmental landscape related to mesic (green)'
+        title += '\n& xeric (brown) conditions. Each side has 10 plots in 2 rows.'
+        plt.title(title, fontsize=13)
+
+    if envDiff == 'same':
+        title = 'Birds-eye view of the environmental landscape related to mesic (green)'
+        title += '\n conditions. Each side of the simulated area has 10 plots in 2 rows.'
+        plt.title(title, fontsize=13)
+    
+            
+    # A SECOND PLOT
+    fig.add_subplot(2, 1, 2)
+    
+    if envDiff == 'differ':
+        DN = funx.get_kdens(Nx)
+        plt.plot(DN[0], DN[1], color = 'brown', lw=3, label= 'North', alpha=0.5)
+        plt.fill_between(DN[0], DN[1], 0, color = 'brown', alpha = 0.3)
+                
+    DS = funx.get_kdens(Sx)
+    plt.plot(DS[0], DS[1], color = 'green', lw=3, label = 'South', alpha=0.5)
+    plt.fill_between(DS[0], DS[1], 0, color = 'green', alpha = 0.3)
+    
+    plt.xlabel('Environmental Condition', fontsize=12)
+    plt.ylabel('Frequency', fontsize=12)
+    
+    txt =  'Distribution(s) of environmental values across patches.'
+    txt += 'Mismatch\nof species optima influences the probability of'
+    txt += 'reproduction,\ndeath, and transitions to and from dormancy.'
+    
+    plt.xlim(0, 1)
+    plt.ylim(0, 8)
+    plt.text(0.05, 5.2, txt, fontsize=12)
+    
+    #plt.savefig('/Users/lisalocey/Desktop/EC_'+envDiff+'.png', 
+    #dpi=600,bbox_inches='tight',pad_inches=0.1)
+    
+    plt.subplots_adjust( wspace=0.0, hspace=0.4)
+    plt.show()    
+    
+    return [NRowYs, NRow1Xs, NRow2Xs, SRowYs, SRow1Xs,
+                SRow2Xs, Ncounts, Nverts, Scounts, Sverts]
+                
+
+_lists = ECfig(envdiff='differ')
