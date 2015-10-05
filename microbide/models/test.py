44ec3eb317e5267d3781498261d81a06da984010
Mario Muscarella <mmuscare@indiana.edu>
2015-10-05 15:02:56 -0400

    minor change
    Former-commit-id: 3640a768c258db5e47b52d8203286ce5704a1344

--- microbide/models/test.py
+++ microbide/models/test.py
@@ -1,13 +1,13 @@
 from __future__ import division
-import sys, csv, os, multiprocessing
-# import EClandscape as land
-# import ECMicrobideCore as model
-# import ECfunctions as funx
+import sys, csv, os, time
+import multiprocessing # Python tool for parallelizing
+import EClandscape as land
+import ECMicrobideCore as model
+import ECfunctions as funx
 
 mypath = os.path.dirname(os.path.realpath(__file__))
-path = os.path.join(mypath, 'SbyS')
-print(path)
-
-
+path = os.path.join(os.path.split(mypath)[0], 'SbyS')
 cores = multiprocessing.cpu_count()
+
+print('Output Path = '+str(path))
 print('Available Cores = '+str(cores))
