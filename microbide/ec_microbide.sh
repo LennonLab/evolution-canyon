db74454d57193e0c28d0e3311836a58c0ac6e55b
Mario Muscarella <mmuscare@indiana.edu>
2015-10-30 09:54:36 -0400

    error in sh file that looked for the wrong bide model
    Former-commit-id: 08576e8585d12008266b74cb80d8a1efe26766f6

--- microbide/ec_microbide.sh
+++ microbide/ec_microbide.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -k o
-#PBS -l nodes=1:ppn=8,vmem=25gb,walltime=24:00:00
-#PBS -M mmuscare@indiana.edu,lennonj@indiana.edu,kjlocey@indiana.edu
+#PBS -l nodes=1:ppn=8,vmem=100gb,walltime=24:00:00
+#PBS -M mmuscare@indiana.edu
 #PBS -m abe
 #PBS -j oe
 #PBS -o /N/dc2/projects/Lennon_Sequences/2015_EvolutionCanyonBIDE/
@@ -10,4 +10,4 @@ module load intel
 module load python
 now=$(date +"%Y%m%d")
 cd /N/dc2/projects/Lennon_Sequences/2015_EvolutionCanyonBIDE/
-python ./models/ECmodelP.py > ./logs/ECmodel_$now.log
+python ./models/ECmodel_MultiProc.py > ./logs/ECmodel_$now.log
